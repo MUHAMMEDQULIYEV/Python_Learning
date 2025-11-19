37db92396c1679c82896840a193e4cbc53a646da
MUHAMMEDQULIYEV <quliyevmuhammad405@gmail.com>
2025-11-19 23:15:41 +0400

    Revert "learning @property decorator"
    This reverts commit 4758d4fbbcf2765ecd06dc231d84aeae5eb751bd.

--- decor.py
+++ decor.py
@@ -24,22 +24,3 @@ def display_info(name, age):
 
 
 display_info("john", 47)
-
-
-class decorator_class(object):
-    def __init__(self, original_function):
-        self.original_function = original_function
-
-    def __call__(self, *args, **kwargs):
-        print(f"wrapper executed before {self.originial_function.__name__}.")
-        return self.originial_function(*args, **kwargs)
-
-
-@decorator_class
-def display():
-    print("display function run and learn this topic")
-
-
-@decorator_class
-def display_info(name, age):
-    print("display_into run   with argument ({},{})".format(name, age))
